3bb4e55943525f3f6a470ff6f4407b25b42b67d3
lynnfaraday <lynn@wordsmyth.org>
2018-01-25 22:55:43 -0500

    Yaml editor with warnings.

---
+++ app/components/yaml-editor.js
@@ -0,0 +1,20 @@
+import Component from '@ember/component';
+import JsYaml from "npm:js-yaml";
+
+export default Component.extend({
+    text: '',
+    warning: '',
+    
+    actions: {
+        
+        textChanged: function(evt) {
+            this.set('warning', '');
+            try {
+                let yaml = JsYaml.safeLoad(evt.target.value);
+            }
+            catch(error) {
+                this.set('warning', `There may be a problem with your configuration YAML text: ${error.reason}.`);
+            }
+        },
+    }
+});
--- app/controllers/config.js
+++ app/controllers/config.js
@@ -1,23 +1,11 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import JsYaml from "npm:js-yaml";
 
 export default Controller.extend({    
     ajax: service(),
     flashMessages: service(),
     
     actions: {
-        
-        textChanged(evt, warning) {
-            try {
-                let yaml = JsYaml.safeLoad(evt.target.value);
-            }
-            catch(error) {
-               this.get('flashMessages').warning(`There may be a problem with your configuration YAML text: ${error.reason}.`) 
-                Ember.set(warning, `There may be a problem with your configuration YAML text: ${error.reason}.`);
-            }
-            
-        },
         
         save() {
             let aj = this.get('ajax');
