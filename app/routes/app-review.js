4fecfca739a8cc5d3d7ba8cce01654f80a581767
Linda Naughton <lynn@wordsmyth.org>
2018-09-08 00:19:20 -0400

    Allow app review from web.

---
+++ app/routes/app-review.js
@@ -0,0 +1,12 @@
+import Route from '@ember/routing/route';
+import DefaultRoute from 'ares-webportal/mixins/default-route';
+import { inject as service } from '@ember/service';
+
+export default Route.extend(DefaultRoute, {
+    gameApi: service(),
+    
+    model: function(params) {
+        let api = this.get('gameApi');
+        return api.requestOne('appReview', { id: params['id'] });
+    }
+});
