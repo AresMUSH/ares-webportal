2c7152ca8d3c18a07316d7534499ff9f5fba8e18
spiritlake <spiritlakemu@gmail.com>
2018-09-22 20:20:14 +0000

    fix offset and use circles for bg skills

--- app/helpers/attr-rating.js
+++ app/helpers/attr-rating.js
@@ -1,12 +1,7 @@
 import { helper } from '@ember/component/helper';
 
 export function attrRating(params, hash) {
-//	console.log(params, hash);
-	var a= (new Array(hash.max)).fill({}).map(function (fill, i) { return { fill: i <= hash.rating }; });
-// console.log(a)
-        return a;
-	//  let text = `${hash.text}`;
-  //  return ansi_up.ansi_to_html(text, { use_classes: true });
+    return (new Array(hash.max)).fill({}).map(function (fill, i) { return { fill: i+1 <= hash.rating }; });
 }
 
 export default helper(attrRating);
