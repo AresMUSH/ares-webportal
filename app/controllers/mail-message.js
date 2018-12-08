f2a9f55c4d87da1e6b698698276e570b6e9fafe4
Linda Naughton <lynn@wordsmyth.org>
2018-12-08 10:51:24 -0500

    Mail tags.

--- app/controllers/mail-message.js
+++ app/controllers/mail-message.js
@@ -6,8 +6,9 @@ export default Controller.extend({
   flashMessages: service(),
   subject: '',
   message: '',
-  to_list: '',
-  fwd_to_list: '',
+  toList: '',
+  fwdToList: '',
+  newTag: '',
   confirmDelete: false,
 
   resetOnExit: function() {
@@ -16,17 +17,30 @@ export default Controller.extend({
   
   setup: function() {
     this.set('subject', `Re: ${this.get('model.subject')}`);
-    this.set('to_list', this.get('model.from'));
-    this.set('fwd_to_list', '');
+    this.set('toList', this.get('model.from'));
+    this.set('fwdToList', '');
     this.set('message', '');
+    this.set('newTag', '');
   }.observes('model'),
+  
+  saveTags: function() {
+    let api = this.get('gameApi');
+    api.requestOne('tagMail', { id: this.get('model.id'), tags: this.get('model.tags') }, null)
+    .then( (response) => {
+      if (response.error) {
+        return;
+      }
+      this.send('reloadModel');
+      this.get('flashMessages').success('Tags updated!');
+    });
+  },
     
   actions: {
     sendReply: function() {
       let api = this.get('gameApi');
       api.requestOne('sendMail', { subject: this.get('subject'), 
       message: this.get('message'),
-      to_list: this.get('to_list')}, null)
+      to_list: this.get('toList')}, null)
       .then( (response) => {
         if (response.error) {
           return;
@@ -39,7 +53,7 @@ export default Controller.extend({
       let api = this.get('gameApi');
       api.requestOne('sendMail', { subject: `Fwd: ${this.get('model.subject')}`, 
       message: `${this.get('message')}\n\n----\n\nOriginal Message:\n${this.get('model.raw_body')}`,
-      to_list: this.get('fwd_to_list')}, null)
+      to_list: this.get('fwdToList')}, null)
       .then( (response) => {
         if (response.error) {
           return;
@@ -70,6 +84,18 @@ export default Controller.extend({
         this.get('flashMessages').success('Message undeleted.');
       });
     },
+    addNewTag: function() {
+      let tags = this.get('model.tags');
+      let newTag = this.get('newTag');
+      
+      if (newTag === '') {
+        return;
+      }
+      
+      tags.push(newTag);
+      this.set('model.tags', tags);
+      this.saveTags();
+    },
     archiveMsg: function() {
       let api = this.get('gameApi');
       api.requestOne('archiveMail', { id: this.get('model.id') }, null)
@@ -80,6 +106,10 @@ export default Controller.extend({
         this.transitionToRoute('mail');
         this.get('flashMessages').success('Message archived!');
       });
+    },
+    tagsChanged: function(tags) {
+      this.set('model.tags', tags);
+      this.saveTags();
     }
   }
 });
