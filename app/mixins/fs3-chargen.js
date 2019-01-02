559e2e2549d05a1016261b6df08c24b1b810d463
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-01-02 10:44:16 -0500

    Update fs3-chargen.js

--- app/mixins/fs3-chargen.js
+++ app/mixins/fs3-chargen.js
@@ -6,7 +6,7 @@ export default Mixin.create({
         let total = this.countPointsInGroup(this.get('model.char.fs3.fs3_attributes'), 0, 2, 2);
         return total;
     }.property('model.char.fs3.fs3_attributes.@each.rating'),
-    
+
     countPointsInGroup: function(list, free_points, max_free_rating, cost_per_rating) {
         if (!list) {
             return;
@@ -18,19 +18,19 @@ export default Mixin.create({
                 points = points + ((rating - max_free_rating) * cost_per_rating);
             }
         });
-        
+
         return (points <= free_points) ? 0 : (points - free_points);
     },
-    
+
     buildFs3QueryData: function() {
         if (this.get('model.app.game.disabled_plugins.fs3skills')) {
             return {};
         }
-        
+
         let specialties = {};
         let actionSkills = this.get('model.char.fs3.fs3_action_skills')
-        
-        if (actionSkills) {         
+
+        if (actionSkills) {
         actionSkills.forEach(function (ability) {
             if (ability.specialties) {
                 let selectedSpecs = ability.specialties.filter( s => s.selected ).map(s => s.name);
@@ -38,7 +38,7 @@ export default Mixin.create({
                 }
             });
         }
-        
+
         return {
             fs3_attributes: this.createAbilityHash(this.get('model.char.fs3.fs3_attributes')),
             fs3_action_skills: this.createAbilityHash(this.get('model.char.fs3.fs3_action_skills')),
@@ -48,8 +48,8 @@ export default Mixin.create({
             fs3_specialties: specialties
         };
     },
-    
-    
+
+
     createAbilityHash: function(ability_list) {
         if (!ability_list) {
             return {};
@@ -59,7 +59,7 @@ export default Mixin.create({
                 return map;
                 }, {});
     },
-    
+
     skillPoints: function() {
         let total = 0;
         total = total + this.countPointsInGroup(this.get('model.char.fs3.fs3_action_skills'), 0, 1, 1);
@@ -68,13 +68,13 @@ export default Mixin.create({
         total = total + this.countPointsInGroup(this.get('model.char.fs3.fs3_advantages'), 0, 0, this.get('model.cgInfo.fs3.advantages_cost'));
         return total;
     }.property('model.char.fs3.fs3_backgrounds.@each.rating', 'model.char.fs3.fs3_action_skills.@each.rating', 'model.char.fs3.fs3_languages.@each.rating', 'model.char.fs3.fs3_advantages.@each.rating'),
-    
+
     actionPoints: function() {
         let total = 0;
         total = total + this.countPointsInGroup(this.get('model.char.fs3.fs3_action_skills'), 0, 1, 1);
         return total;
     }.property('model.char.fs3.fs3_action_skills.@each.rating'),
-     
+
     checkLimits: function(list, limits, title) {
         if (!list) {
             return;
@@ -89,13 +89,19 @@ export default Mixin.create({
             }
         }
     },
-    
+
     validateChar: function() {
         this.set('charErrors', []);
 
+
         this.checkLimits(this.get('model.char.fs3.fs3_action_skills'), this.get('model.cgInfo.fs3.skill_limits'), 'action skills');
         this.checkLimits(this.get('model.char.fs3.fs3_attributes'), this.get('model.cgInfo.fs3.attr_limits'), 'attributes');
-        
+        // Magic
+        let magic = this.get('model.char.fs3.fs3_attributes.magic');
+        if (magic > 1) {
+            this.charErrors.push(`Magic cannot be higher than 1.`);
+        }
+
         let totalAttrs = this.get('attrPoints');
         let totalSkills = this.get('skillPoints');
         let totalAction = this.get('actionPoints');
@@ -108,25 +114,25 @@ export default Mixin.create({
         if (totalAction > maxAction) {
             this.charErrors.push(`You can only spend ${maxAction} points in action skills.  You have spent ${totalAction}.`);
         }
-        
+
         let maxAp = this.get('model.cgInfo.fs3.max_ap');
         let totalAp = totalAttrs + totalSkills;
         if (totalAp > maxAp) {
             this.charErrors.push(`You can only spend ${maxAp} ability points.  You have spent ${totalAp}.`);
         }
-        
+
         this.toggleCharChanged();
     },
-    
+
     actions: {
         addBackgroundSkill() {
-            this.get('model.char.fs3.fs3_backgrounds').pushObject( Ember.Object.create( { name: "Enter Skill Name" , rating: 1, rating_name: 'Fair' }) );  
+            this.get('model.char.fs3.fs3_backgrounds').pushObject( Ember.Object.create( { name: "Enter Skill Name" , rating: 1, rating_name: 'Fair' }) );
             this.validateChar();
         },
-        
+
         abilityChanged() {
             this.validateChar();
         }
     }
-    
+
 });
