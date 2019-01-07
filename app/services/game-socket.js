e932d611df65b227a17a5700b5579535b4c37fa9
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-01-06 23:30:41 -0500

    Update game-socket.js

--- app/services/game-socket.js
+++ app/services/game-socket.js
@@ -14,7 +14,7 @@ export default Service.extend({
     sceneCallback: null,
 
     socketUrl() {
-	var protocol = aresconfig.ssl ? 'wss' : 'ws';
+	var protocol = aresconfig.use_https ? 'wss' : 'ws';
         return `${protocol}://${aresconfig.host}:${aresconfig.websocket_port}/websocket`;
     },
 
