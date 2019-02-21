ee9f2192731c2bd644f46361b67ec831c962725b
Linda Naughton <lynn@wordsmyth.org>
2019-02-20 21:24:53 -0500

    Enable fansi web colors.

--- app/routes/application.js
+++ app/routes/application.js
@@ -28,7 +28,7 @@ export default Route.extend(ApplicationRouteMixin, ReloadableRoute, {
             response['socketConnected'] = this.get('gameSocket.connected');
             
             if (response.token_expiry_warning) {
-              this.get('flashMessages').warning(`Your login expires today (in ${response.token_expiry_warning}). You should log out and back in before that happens so you don't lose any work.'`);
+              this.get('flashMessages').warning(`Your login expires today (in ${response.token_expiry_warning}). You should log out and back in before that happens so you don't lose any work.`);
             }
             return response;
         })
