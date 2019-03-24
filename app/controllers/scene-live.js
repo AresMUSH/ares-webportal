71ea78cb909f316119b70bc200d49b12cf3ffa58
Linda Naughton <lynn@wordsmyth.org>
2019-03-24 09:23:58 -0400

    Better scene updates in live scene.

--- app/controllers/scene-live.js
+++ app/controllers/scene-live.js
@@ -13,21 +13,48 @@ export default Controller.extend(AuthenticatedController, {
         let splitMsg = msg.split('|');
         let sceneId = splitMsg[0];
         let char = splitMsg[1];
-        let poseData = splitMsg[2];
-        // For poses we can just add it to the display.  Other events require a reload.
+        let activityType = splitMsg[2];
+        let activityData = splitMsg[3];
+        let notify = true;
+        
         if (sceneId === this.get('model.scene.id')) {
-          if (poseData) {
-            poseData = JSON.parse(poseData);
+          if (activityType == 'new_pose') {
+            let poseData = JSON.parse(activityData);
             let poses = this.get('model.scene.poses');
             if (!poseData.can_edit && (poseData.char.id == this.get('session.data.authenticated.id'))) {
-              poseData.can_edit = true
-              poseData.can_delete = true
+              poseData.can_edit = true;
+              poseData.can_delete = true;
             }
             poses.pushObject(poseData);
-            this.get('gameSocket').notify('New scene activity!');
-            this.scrollSceneWindow();
+          } else if (activityType == 'pose_updated') {
+            let poseData = JSON.parse(activityData);
+            let poses = this.get('model.scene.poses');
+            poses.forEach((p, i) => {
+              if (p.id === poseData.id) {
+                Ember.set(p, 'pose', poseData.pose);
+                Ember.set(p, 'raw_pose', poseData.raw_pose);
+              }
+            });
+            notify = false;
+          }
+          else if (activityType == 'pose_deleted') {
+            let poseData = JSON.parse(activityData);
+            let poses = this.get('model.scene.poses');
+            poses.forEach(p => {
+              if (p.id === poseData.id) {
+                poses.removeObject(p);
+              }
+            });
+            notify = false;
+          } else if (activityType == 'location_updated') {
+            let locationData = JSON.parse(activityData);
+            this.set('model.scene.location', locationData);
+            notify = false;
           } else {
             this.set('model.scene.reload_required', true);            
+          }
+          
+          if (notify) {
             this.get('gameSocket').notify('New scene activity!');
             this.scrollSceneWindow();
           }
