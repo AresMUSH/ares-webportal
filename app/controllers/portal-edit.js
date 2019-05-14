c8e93247843b2318aa30e7c4aa651753ff3ffb8f
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-05-14 17:16:26 -0400

    Update portal-edit.js

--- app/controllers/portal-edit.js
+++ app/controllers/portal-edit.js
@@ -6,13 +6,16 @@ export default Controller.extend({
     gameApi: service(),
 
     actions: {
+        gmsChanged(new_gms) {
+          this.set('model.portal.gms', new_gms);
+        },
         save() {
             let api = this.get('gameApi');
             api.requestOne('portalEdit', {
                id: this.get('model.portal.id'),
                name: this.get('model.portal.name'),
-               gms: this.get('model.portal.gms'),
-               location: this.get('model.portal.portal'),
+               gms: (this.get('model.portal.gms') || []).map(gm => gm.name),
+               location: this.get('model.portal.location'),
                primary_school: this.get('model.portal.primary_school'),
                all_schools: this.get('model.portal.all_schools'),
                description: this.get('model.portal.description'),
