da0b328087489c5764727efbd78523a6c39c3a3b
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-05-29 17:42:42 -0400

    Secrets page

--- app/router.js
+++ app/router.js
@@ -101,6 +101,7 @@ Router.map(function() {
   this.route('search-help');
   this.route('search-forum');
   this.route('search-spells');
+  this.route('secrets');
   this.route('setup');
   this.route('setup-colors');
   this.route('shutdown');
