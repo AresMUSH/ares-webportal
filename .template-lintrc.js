9bc92234fb027304c1defb5ea579e1da6adcc4fd
Linda Naughton <lynn@wordsmyth.org>
2019-06-15 10:54:50 -0400

    Missed a few files.

---
+++ .template-lintrc.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  extends: 'recommended',
+  rules: {
+    'block-indentation': false,
+    'quotes': false,
+    'no-triple-curlies': false,
+    'table-groups': false,
+    'no-unnecessary-concat': false,
+    'no-html-comments': false,
+    'no-nested-interactive': false
+  },
+};
