67a333f9dadbb0392c32ab15ea67a6e66c0a3412
Linda Naughton <lynn@wordsmyth.org>
2019-09-26 00:43:31 -0400

    Achievement notifications.

--- app/controllers/notifications.js
+++ app/controllers/notifications.js
@@ -1,7 +1,8 @@
 import Controller from '@ember/controller';
+import AuthenticatedController from 'ares-webportal/mixins/authenticated-controller';
 import { inject as service } from '@ember/service';
 
-export default Controller.extend({
+export default Controller.extend(AuthenticatedController, {
     gameApi: service(),
 
     actions: {
