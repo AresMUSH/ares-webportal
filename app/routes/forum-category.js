ea45cf4879502f6d8f1bec3ffabe13f9b82d6866
Linda Naughton <lynn@wordsmyth.org>
2019-11-23 16:51:19 -0500

    Live forum updates.

--- app/routes/forum-category.js
+++ app/routes/forum-category.js
@@ -4,7 +4,19 @@ import DefaultRoute from 'ares-webportal/mixins/default-route';
 
 export default Route.extend(DefaultRoute, {
     gameApi: service(),
-    
+    gameSocket: service(),
+  
+    activate: function() {
+        this.controllerFor('forum-category').setupCallback();
+        $(window).on('beforeunload', () => {
+            this.deactivate();
+        });
+    },
+
+    deactivate: function() {
+        this.gameSocket.removeCallback('new_forum_activity');
+    },
+  
     model: function(params) {
         let api = this.gameApi;
         return api.requestOne('forumCategory', {category_id: params['category_id']});
--- app/routes/forum.js
+++ app/routes/forum.js
@@ -5,8 +5,20 @@ import DefaultRoute from 'ares-webportal/mixins/default-route';
 
 export default Route.extend(DefaultRoute, ReloadableRoute, {
     gameApi: service(),
+    gameSocket: service(),
     session: service(),
     
+    activate: function() {
+        this.controllerFor('forum').setupCallback();
+        $(window).on('beforeunload', () => {
+            this.deactivate();
+        });
+    },
+
+    deactivate: function() {
+        this.gameSocket.removeCallback('new_forum_activity');
+    },
+  
     model: function() {
         let api = this.gameApi;
         return api.requestOne('forumList');
