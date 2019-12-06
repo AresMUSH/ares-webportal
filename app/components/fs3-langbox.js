ab362faebdba3e1548b6ba515ee00784ccdb75b5
spiritlake <32268357+spiritlake@users.noreply.github.com>
2019-12-06 15:09:25 -0500

    Update fs3-langbox.js

--- app/components/fs3-langbox.js
+++ app/components/fs3-langbox.js
@@ -9,7 +9,7 @@ export default Component.extend({
 
         switch (this.rating) {
             case 0:
-                name = "Everyman";
+                name = "Incapable";
                 break;
             case 1:
                 name = "Beginner";
