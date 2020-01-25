408dd20da22c1197fcbfda58a8985b5c857b3bd6
xRaerasx <unconfigured@null.spigotmc.org>
2020-01-25 09:52:04 -0500

    Adding Portal Die Roller

--- app/components/live-scene-custom-play.js
+++ app/components/live-scene-custom-play.js
@@ -3,3 +3,36 @@ import Component from '@ember/component';
 export default Component.extend({
   tagName: ''
 });
+
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+
+export default Component.extend({
+  tagName: '',
+  diceString: '',
+  
+  gameApi: service(),
+  flashMessages: service(),
+
+  actions: {
+
+    rollDice() {
+      let api = this.gameApi;
+      let dice = this.diceString;
+      this.set('showRollDice', null);
+      this.set('diceString', '');
+          
+      if (!dice) {
+        this.flashMessages.danger("You haven't specified what dice to roll.");
+        return;
+      }
+
+      api.requestOne('rollDice', { id: this.get('scene.id'), dice_string: dice }, null)
+      .then( (response) => {
+        if (response.error) {
+          return;
+        }
+      });
+    }
+  }
+});
