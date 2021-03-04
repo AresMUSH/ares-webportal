bc1d2c4279129a6d240f485744b8d0cea8ab2197
spiritlake <spiritlakemu@gmail.com>
2021-03-04 12:16:38 -0500

    Moving custom code to custom hooks

--- app/components/char-edit-custom.js
+++ app/components/char-edit-custom.js
@@ -2,16 +2,16 @@ import Component from '@ember/component';
 
 export default Component.extend({
   tagName: '',
-  
+
   didInsertElement: function() {
     let self = this;
     this.set('updateCallback', function() { return self.onUpdate(); } );
   },
-  
+
   onUpdate: function() {
     // Return a hash containing your data.  Character data will be in 'char'.  For example:
-    // 
+    //
     // return { goals: this.get('char.custom.goals') };
-    return {};
+    return {plot_prefs: this.get('char.custom.plot_prefs')};
   }
 });
