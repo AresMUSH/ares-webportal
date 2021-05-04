0ce9b821d1480f2e26a000edc4dc827d31f852da
spiritlake <spiritlakemu@gmail.com>
2021-05-04 12:09:47 -0400

    Play screen changes

--- app/components/live-scene-control.js
+++ app/components/live-scene-control.js
@@ -6,7 +6,7 @@ import AuthenticatedController from 'ares-webportal/mixins/authenticated-control
 
 export default Component.extend(AuthenticatedController, {
     scenePose: '',
-    showSceneDetails: false,
+    // showSceneDetails: false,
     rollString: null,
     confirmDeleteScenePose: false,
     confirmDeleteScene: false,
