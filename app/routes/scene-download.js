6d497de3f0d161b524d663a4012fb7c220cfc42a
Linda Naughton <lynn@wordsmyth.org>
2021-06-26 21:51:56 -0400

    Reset download scenes.

--- app/routes/scene-download.js
+++ app/routes/scene-download.js
@@ -1,9 +1,10 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import ReloadableRoute from 'ares-webportal/mixins/reloadable-route';
+import RouteResetOnExit from 'ares-webportal/mixins/route-reset-on-exit';
 import DefaultRoute from 'ares-webportal/mixins/default-route';
 
-export default Route.extend(ReloadableRoute, DefaultRoute, {
+export default Route.extend(ReloadableRoute, DefaultRoute, RouteResetOnExit, {
     gameApi: service(),
     textFile: null,
   
