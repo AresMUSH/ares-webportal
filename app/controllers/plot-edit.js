22b71530066e01e43b83ef5ff73d3c2e79d8ef1d
Linda Naughton <lynn@wordsmyth.org>
2021-07-05 20:11:16 -0400

    Plot tags.

--- app/controllers/plot-edit.js
+++ app/controllers/plot-edit.js
@@ -12,13 +12,22 @@ export default Controller.extend({
         
         save: function() {
             let api = this.gameApi;
+            
+            let tags = this.get('model.plot.tags') || [];
+            if (!Array.isArray(tags)) {
+                tags = tags.split(/[\s,]/);
+            }
+            
             api.requestOne('editPlot', { id: this.get('model.plot.id'),
                title: this.get('model.plot.title'), 
                summary: this.get('model.plot.summary'),
                storytellers: (this.get('model.plot.storytellers') || []).map(storyteller => storyteller.name),
                completed: this.get('model.plot.completed'),
                content_warning: this.get('model.plot.content_warning'),
-               description: this.get('model.plot.description')}, null)
+               description: this.get('model.plot.description'),
+               tags: tags
+            
+            }, null)
             .then( (response) => {
                 if (response.error) {
                     return;
