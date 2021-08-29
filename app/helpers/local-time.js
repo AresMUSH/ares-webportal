5bc7526b7c230266f0d1603b086da2abf4bbcb84
Linda Naughton <lynn@wordsmyth.org>
2021-08-29 08:35:59 -0400

    Local time fix for dayjs, dropup menus on play screen.

--- app/helpers/local-time.js
+++ app/helpers/local-time.js
@@ -2,7 +2,8 @@ import dayjs from 'dayjs';
 import { helper } from '@ember/component/helper';
 
 export function localTime(time) {
-    return dayjs.utc(time).local().format('hh:mm A');
+//    return dayjs.utc(time).local().format('hh:mm A');
+return dayjs(time).format('hh:mm A');
 }
 
 export default helper(localTime);
