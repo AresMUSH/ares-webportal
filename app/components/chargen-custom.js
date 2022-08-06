f0312dab9de5d4d7bea3da52fb42c9ce87ac2856
aerrin99 <102542459+aerrin99@users.noreply.github.com>
2022-08-05 23:24:09 -0400

    create mounts in cg

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -21,6 +21,10 @@ export default Component.extend({
     spellsChanged(new_spells) {
       this.set('char.custom.starting_spells', new_spells);
         
+    },
+    mounttypeChanged(type) {
+      this.set('char.custom.mount_type', type);
+        
     }
   },
   
@@ -31,7 +35,12 @@ export default Component.extend({
     return {
       major_school: this.get('char.custom.major_school'),
       minor_school: this.get('char.custom.minor_school'),
-      starting_spells: this.get('char.custom.starting_spells')
+      starting_spells: this.get('char.custom.starting_spells'),
+      mount_type: this.get('char.custom.mount_type'), 
+      mount_name: this.get('char.custom.mount_name'),
+      mount_shortdesc: this.get('char.custom.mount_shortdesc'),
+      mount_desc: this.get('char.custom.mount_desc'),
+
     };
   }
 });
