7f8ec65b9b8ae9c8ecfd9aebecea132edd945100
aerrin99 <102542459+aerrin99@users.noreply.github.com>
2022-08-30 12:15:30 -0400

    splitting mage and mythic spells

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -2,7 +2,7 @@ import Component from '@ember/component';
 
 export default Component.extend({
   tagName: '',
-  
+
   didInsertElement: function() {
     this._super(...arguments);
     let self = this;
@@ -12,34 +12,39 @@ export default Component.extend({
   actions: {
     majorschoolChanged(new_school) {
       this.set('char.custom.major_school', new_school);
-        
+
     },
     minorschoolChanged(new_school) {
       this.set('char.custom.minor_school', new_school);
-        
+
     },
-    spellsChanged(new_spells) {
-      this.set('char.custom.starting_spells', new_spells);
-        
+    mageSpellsChanged(new_spells) {
+      this.set('char.custom.mage_starting_spells', new_spells);
+
+    },
+    mythicSpellsChanged(new_spells) {
+      this.set('char.custom.mythic_starting_spells', new_spells);
+
     },
     mounttypeChanged(type) {
       this.set('char.custom.mount_type', type);
-        
+
     },
     lorehookPrefChanged(val) {
       this.set('char.custom.lore_hook_pref', val);
     }
   },
-  
+
   onUpdate: function() {
     // Return a hash containing your data.  Character data will be in 'char'.  For example:
-    // 
+    //
     // return { goals: this.get('char.custom.goals') };
     return {
       major_school: this.get('char.custom.major_school'),
       minor_school: this.get('char.custom.minor_school'),
-      starting_spells: this.get('char.custom.starting_spells'),
-      mount_type: this.get('char.custom.mount_type'), 
+      mage_starting_spells: this.get('char.custom.mage_starting_spells'),
+      mythic_starting_spells: this.get('char.custom.mythic_starting_spells'),
+      mount_type: this.get('char.custom.mount_type'),
       mount_name: this.get('char.custom.mount_name'),
       mount_shortdesc: this.get('char.custom.mount_shortdesc'),
       mount_desc: this.get('char.custom.mount_desc'),
