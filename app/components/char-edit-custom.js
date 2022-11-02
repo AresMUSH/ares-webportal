4d090a1f28343445b3e6bfe334a093519f7cf998
aerrin99 <102542459+aerrin99@users.noreply.github.com>
2022-11-02 17:08:48 -0400

    edit mounts on web

--- app/components/char-edit-custom.js
+++ app/components/char-edit-custom.js
@@ -2,17 +2,22 @@ import Component from '@ember/component';
 
 export default Component.extend({
   tagName: '',
-  
+
   didInsertElement: function() {
     this._super(...arguments);
     let self = this;
     this.set('updateCallback', function() { return self.onUpdate(); } );
   },
-  
+
   onUpdate: function() {
     // Return a hash containing your data.  Character data will be in 'char'.  For example:
-    // 
+    //
     // return { goals: this.get('char.custom.goals') };
-    return {};
+    return {
+      mythic_name: this.get('char.custom.mythic_name'),
+      mythic_about: this.get('char.custom.mythic_about'),
+      mythic_desc: this.get('char.custom.mythic_desc'),
+      mythic_shortdesc: this.get('char.custom.mythic_shortdesc'),
+    };
   }
 });
