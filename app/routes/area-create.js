5253ceb758f250d649055f5d82ffbb3495063eee
Linda Naughton <lynn@wordsmyth.org>
2023-03-05 22:07:56 -0500

    Create areas, add area parents.

---
+++ app/routes/area-create.js
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import AuthenticatedRoute from 'ares-webportal/mixins/authenticated-route';
+import RouteResetOnExit from 'ares-webportal/mixins/route-reset-on-exit';
+
+export default Route.extend(AuthenticatedRoute, RouteResetOnExit, {
+    gameApi: service(),
+    
+    model: function(params) {
+        let api = this.gameApi;
+        return api.requestOne('manageLocations');
+    }
+});
