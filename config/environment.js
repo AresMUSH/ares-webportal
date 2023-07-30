95a411fff535b4fc530a54f950585fe40baf9c4a
Linda Naughton <lynn@wordsmyth.org>
2023-07-30 18:07:51 -0400

    Ember 4.10

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 'use strict';
 
 module.exports = function (environment) {
-  let ENV = {
+  const ENV = {
       pageTitle: {
             replace: true
           },
@@ -19,6 +19,7 @@ module.exports = function (environment) {
         // Here you can enable experimental features on an ember canary build
         // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
       },
+      EXTEND_PROTOTYPES: true,
     },
 
     APP: {
