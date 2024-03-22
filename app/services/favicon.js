a2b0e64be425b2eae76bfa8381335d9033b14d17
aerrin99 <aerrin99@gmail.com>
2024-03-22 13:41:21 +0000

    rolling some files back to vanilla

--- app/services/favicon.js
+++ app/services/favicon.js
@@ -1,27 +1,15 @@
-import $ from 'jquery';
+import $ from "jquery"
 import Service from '@ember/service';
 
-var blinker = null;
-var currSrc = null;
-
 export default Service.extend({
+    
   changeFavicon(active) {
-    var src = '/game/uploads/theme_images/favicon.ico';
-    var active_src = '/game/uploads/theme_images/favicon-active.ico';
-    if (active) {
-      if (blinker) {
-        return;
-      }
-
-      currSrc = src;
-      blinker = setInterval(() => {
-        currSrc = currSrc === src ? active_src : src;
-        $('link[rel="shortcut icon"]').attr('href', currSrc);
-      }, 100);
-    } else {
-      clearInterval(blinker);
-      blinker = null;
-      $('link[rel="shortcut icon"]').attr('href', src);
-    }
-  },
+        var src = '/game/uploads/theme_images/favicon.ico';
+        if (active) { 
+            src = '/game/uploads/theme_images/favicon-active.ico';
+        }
+        $('link[rel="shortcut icon"]').attr('href', src);
+        
+    },
+     
 });
