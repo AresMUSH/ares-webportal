913eab00ed7655e47e5bb4afba4ea0a66aa76288
aerrin99 <aerrin99@gmail.com>
2024-04-03 12:31:27 -0400

    Update live-scene-custom-play.js

--- app/components/live-scene-custom-play.js
+++ app/components/live-scene-custom-play.js
@@ -13,34 +13,36 @@ export default Component.extend({
   actions: {
 
     addSceneSpell() {
-        let api = this.get('gameApi');
+      let api = this.gameApi;
 
-          // Needed because the onChange event doesn't get triggered when the list is
-          // first loaded, so the roll string is empty.
-        let spellString = this.get('spellString') || this.get('spells')[0];
-        let mod = this.get('mod');
-        let targetName = this.get('targetName');
+        // Needed because the onChange event doesn't get triggered when the list is
+        // first loaded, so the roll string is empty.
+      let spellString = this.spellString || this.spells[0];
+      let mod = this.mod;
+      let targetName = this.targetName;
 
-          if (!spellString) {
-              this.get('flashMessages').danger("You haven't selected a spell to cast.");
-              return;
-          }
-          this.set('selectCastSpell', false);
-          this.set('spellString', null);
-          this.set('targetName', null);
-          this.set('mod', null);
+        if (!spellString) {
+            this.flashMessages.danger("You haven't selected a spell to cast.");
+            return;
+        }
+        this.set('selectCastSpell', false);
+        this.set('spellString', null);
+        this.set('targetName', null);
+        this.set('mod', null);
 
-          api.requestOne('addSceneSpell', { scene_id: this.get('scene.id'),
-              target_name: targetName,
-              mod: mod,
-              spell_string: spellString }, null)
+        api.requestOne('addSceneSpell', {
+          scene_id: this.scene.id,
+          target_name: targetName,
+          mod: mod,
+          spell_string: spellString
+        }, null)
 
-          .then( (response) => {
-              if (response.error) {
-                this.get('flashMessages').danger(response.error);
-                  return;
-              }
-          });
+        .then( (response) => {
+            if (response.error) {
+              this.flashMessages.danger(response.error);
+                return;
+            }
+        });
       }
 
 
