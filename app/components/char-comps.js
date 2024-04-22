12a76f57107f850a96dff775ffcb273e3d646f4f
aerrin99 <aerrin99@gmail.com>
2024-04-22 11:48:30 -0400

    Turn comps back on

--- app/components/char-comps.js
+++ app/components/char-comps.js
@@ -1,21 +1,4 @@
-import EmberObject, { computed } from '@ember/object';
 import Component from '@ember/component';
-import { inject as service } from '@ember/service';
-
 
 export default Component.extend({
-    compFilters: function() {
-        let base_filters = ['Recent', 'All'];
-        return base_filters;
-    }.computed('comps'),
-
-    actions: {
-        filterChanged(newFilter) {
-          this.sendAction('filterChanged', newFilter);
-        },
-        goToPage(newPage) {
-          this.sendAction('goToPage', newPage);
-        }
-    }
-
 });
