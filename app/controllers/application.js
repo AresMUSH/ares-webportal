1612805262ad09090b730e6f5fdc7b0a3fa8929c
lynnfaraday <lynnfaraday@users.noreply.github.com>
2024-12-17 20:18:05 -0500

    Version check no longer warns of patch differences.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -74,7 +74,12 @@ export default Controller.extend(AuthenticatedController, AvailableRoutes, AresC
     }),
     
     versionWarning: computed('mushVersion', 'portalVersion', function() {
-      return this.mushVersion != this.portalVersion;
+      const mushSplit = (this.mushVersion || "").split(".");
+      const portalSplit = (this.portalVersion || "").split(".");
+      if (mushSplit.length < 2 || portalSplit.length < 2 || (mushSplit[0] != portalSplit[0]) || (mushSplit[1] != portalSplit[1])) {
+        return true;
+      }
+      return false;
     }),
     
     checkRoute: function(menu, availableRoutes) {
