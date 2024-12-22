85e7f0c511233aeea4e9d6953eb93b4331edf9d5
lynnfaraday <lynnfaraday@users.noreply.github.com>
2024-12-22 14:09:20 -0500

    Alt switcher actions.

--- app/components/global-navbar.js
+++ app/components/global-navbar.js
@@ -1,16 +1,21 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default Component.extend({
   session: service(),
   showAltSelection: false,
   
-  actions: {
-    switchAlt: function(alt) {
-      this.session.authenticate('authenticator:ares', { name: alt, password: 'ALT' })
-       .then(() => {
-         window.location.replace('/');
-       });
-    }
+  @action
+  setAltSelectionVisible(visible) {
+    this.set('showAltSelection', visible);
+  },
+  
+  @action
+  switchAlt(alt) {
+    this.session.authenticate('authenticator:ares', { name: alt, password: 'ALT' })
+     .then(() => {
+       window.location.replace('/');
+     });
   }
 });
